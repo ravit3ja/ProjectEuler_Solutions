3f820c45cff1b25f111c70c3fe3bccd693f74ca6
MohanRajesh <rajesh.komatlapalli@gmail.com>
2015-06-23 12:58:14 +0530

    Revert "Revert "Project Euler Solutions""
    This reverts commit 9749982778a3d2cd993ae777be5edb0a99e9e86f.

---
+++ EulerSolution.java
@@ -0,0 +1,14 @@
+/* 
+ * Base class for Project Euler Java solutions
+ * by Project Nayuki
+ * 
+ * http://www.nayuki.io/page/project-euler-solutions
+ * https://github.com/nayuki/Project-Euler-solutions
+ */
+
+
+public interface EulerSolution {
+	
+	public String run();
+	
+}
---
+++ p009.hs
@@ -0,0 +1,15 @@
+{- 
+ - Solution to Project Euler problem 9
+ - by Project Nayuki
+ - 
+ - http://www.nayuki.io/page/project-euler-solutions
+ - https://github.com/nayuki/Project-Euler-solutions
+ -}
+
+
+p = 1000
+main = putStrLn (show ans)
+ans = head [a * b * (p - a - b) | a <- [1..p], b <- [1..p], isRight a b]
+isRight a b = a <= b && b <= c
+	&& a * a + b * b == c * c
+	where c = p - a - b
